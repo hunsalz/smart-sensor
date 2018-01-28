25e31b5936c299f8ff6b91b732de05bbe48fd5d8
hunsalz <info@hunsalz.de>
2018-01-28 23:43:51 +0100

    eased Firebase handling

--- Esp8266/Esp8266.cpp
+++ Esp8266/Esp8266.cpp
@@ -15,6 +15,13 @@ void Esp8266::begin() {
   // setup MDNS
   MDNS_CFG.begin("esp8266");
   MDNS.addService("http", "tcp", PORT);
+
+  _mqttService.begin("m21.cloudmqtt.com", 13444);
+  _mqttService.getMqttClient().setCredentials(MQTT_USER, MQTT_PASSWD);
+
+  // setup Firebase connection
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
   // init file system to handle static web resources
   FILESYSTEM.begin();
   // setup web server
@@ -51,32 +58,30 @@ void Esp8266::begin() {
   });
   // setup loop interval
   SYS_CFG.setLoopInterval(2000);
-  // setup Firebase connection
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   // setup logger settings
   //_logger.getAppender().push_back(new RollingFileAppender(LOG_FILENAME, 40,
   // 1024, true));
   LOG.addLevelToAll(Appender::VERBOSE);
 
-  LOG.addFormatterToAll([](Print &output, Appender::Level level, const char *msg, va_list *args) {
+  LOG.addFormatterToAll(
+      [](Print &output, Appender::Level level, const char *msg, va_list *args) {
 
-    // output log level
-    output.print(Appender::toString(level, true));
-    output.print(Appender::DEFAULT_SEPARATOR);
-    // output uptime of this program in milliseconds
-    output.print(millis());
-    output.print(Appender::DEFAULT_SEPARATOR);
-    // output free heap space
-    output.print(ESP.getFreeHeap());
-    output.print(Appender::DEFAULT_SEPARATOR);
-    // determine buffer length for formatted data
-    size_t length = vsnprintf(NULL, 0, msg, *args) + 1;
-    char buffer[length];
-    // output formatted data
-    vsnprintf(buffer, length, msg, *args);
-    output.print(buffer);
-  });
-
+        // output log level
+        output.print(Appender::toString(level, true));
+        output.print(Appender::DEFAULT_SEPARATOR);
+        // output uptime of this program in milliseconds
+        output.print(millis());
+        output.print(Appender::DEFAULT_SEPARATOR);
+        // output free heap space
+        output.print(ESP.getFreeHeap());
+        output.print(Appender::DEFAULT_SEPARATOR);
+        // determine buffer length for formatted data
+        size_t length = vsnprintf(NULL, 0, msg, *args) + 1;
+        char buffer[length];
+        // output formatted data
+        vsnprintf(buffer, length, msg, *args);
+        output.print(buffer);
+      });
 
   LOG.verbose(F("========================="));
   LOG.verbose(F("Setup finished. Have fun."));
@@ -92,19 +97,13 @@ void Esp8266::run() {
     _dht22.update();
     _mq135.update();
 
-    Firebase.set("BMP280", JsonVariant(_bmp280.getJsonValue()));
+    write("BMP280", _bmp280.getJsonValue());
+    write("DHT22", _dht22.getJsonValue());
+    write("MQ135", _mq135.getJsonValue());
 
     // Firebase.setFloat("BMP280_Temperature", _bmp280.getTemperature());
     // Firebase.setFloat("BMP280_Pressure", _bmp280.getPressure());
     // Firebase.setFloat("BMP280_Altitude", _bmp280.getAltitude());
-
-    // handle error
-    if (Firebase.failed()) {
-      // TODO log error code
-      _logger.error(F("Saving BMP280 temperature failed."));
-    } else {
-      _logger.verbose(F("BMP280 Temperature|%.2f"), _bmp280.getTemperature());
-    }
 
     // Firebase.setFloat("DHT22_Temperature", _dht22.getTemperature());
     // Firebase.setFloat("DHT22_Humidity", _dht22.getHumidity());
@@ -112,18 +111,21 @@ void Esp8266::run() {
     // Firebase.setFloat("MQ135_PPM", _mq135.getPPM());
     // Firebase.setFloat("MQ135_CO2", _mq135.getCO2());
 
-    LOG.verbose(esp8266util::toString(_bmp280.getJsonValue()));
-    LOG.verbose(esp8266util::toString(_dht22.getJsonValue()));
-    LOG.verbose(esp8266util::toString(_mq135.getJsonValue()));
-
-    // _mqttService.publish("weather-station", getLastSensorValues());
+    _mqttService.publish("weather-station", _bmp280.getJsonValue());
 
     // SYS_CFG.deepSleep();
   }
 }
 
+void Esp8266::write(const char *name, JsonObject &json) {
+  LOG.verbose(F("%s|%s"), name, esp8266util::toString(json).c_str());
+  Firebase.set(name, JsonVariant(json));
+  if (Firebase.failed()) {
+    _logger.error(F("Saving %s values to Firebase failed."), name);
+  }
+}
+
 JsonArray &Esp8266::getLastSensorValues() {
-  
   // TODO - FIXME
   DynamicJsonBuffer jsonBuffer;
   JsonArray &json = jsonBuffer.createArray();
