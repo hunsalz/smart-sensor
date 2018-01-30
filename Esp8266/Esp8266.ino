934fd69f78f3625be64c863b65e773084ad03438
hunsalz <info@hunsalz.de>
2018-01-31 00:04:17 +0100

    reduced code base to single ino-file

--- Esp8266/Esp8266.ino
+++ Esp8266/Esp8266.ino
@@ -1,22 +1,124 @@
-#include "Esp8266.h"
+#include <Esp8266Utils.h>    // https://github.com/hunsalz/esp8266utils
+#include <FirebaseArduino.h> // https://github.com/firebase/firebase-arduino
+#include <Log4Esp.h>         // https://github.com/hunsalz/log4Esp
 
-Esp8266 *esp8266;
+
+
+// web server settings
+const static int PORT = 80;
+
+esp8266util::BMP280Sensor _bmp280;
+esp8266util::DHTSensor _dht22;
+esp8266util::MQ135Sensor _mq135;
 
 void setup() {
+
+  // logger setup
+  LOG.addLevelToAll(Appender::VERBOSE);
+  LOG.addFormatterToAll([](Print &output, Appender::Level level, const char *msg, va_list *args) {
+    // output log level
+    output.print(Appender::toString(level, true));
+    output.print(Appender::DEFAULT_SEPARATOR);
+    // output uptime of this program in milliseconds
+    output.print(millis());
+    output.print(Appender::DEFAULT_SEPARATOR);
+    // output free heap space
+    output.print(ESP.getFreeHeap());
+    output.print(Appender::DEFAULT_SEPARATOR);
+    // determine buffer length for formatted data
+    size_t length = vsnprintf(NULL, 0, msg, *args) + 1;
+    char buffer[length];
+    // output formatted data
+    vsnprintf(buffer, length, msg, *args);
+    output.print(buffer);
+  });
+
+  // serial setup
   Serial.begin(115200);
   Serial.setDebugOutput(false);
-  while (!Serial && !Serial.available()) {
-  };
+  while (!Serial && !Serial.available()) {};
   Serial.println();
-
   LOG.verbose(F("Serial baud rate is [%d]"), Serial.baudRate());
 
-  esp8266 = new Esp8266();
-  esp8266->begin();
+  // sensor setup
+  _bmp280.begin();
+  _dht22.begin(2, 22);
+  _mq135.begin(A0);
+
+  // WiFi setup
+  WIFI_STA_CFG.addAP(WIFI_SSID_1, WIFI_PASSWD_1);
+  WIFI_STA_CFG.addAP(WIFI_SSID_2, WIFI_PASSWD_2);
+  WIFI_STA_CFG.begin();
+  
+  // MDNS setup
+  MDNS_CFG.begin("esp8266");
+  MDNS.addService("http", "tcp", PORT);
+
+  // Firebase setup
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+  // increase loop interval
+  SYS_CFG.setLoopInterval(2000);
+
+  // file system setup
+  FILESYSTEM.begin();
+
+  // web server setup
+  //SERVER.begin(PORT);
+//   // rewrite root context˘˘
+//   SERVER.getWebServer().rewrite("/", "/index.build.html");
+//   // handle static web resources
+//   SERVER.getWebServer().serveStatic("/", SPIFFS, "/www/", "max-age:15");  // cache-control 15 seconds
+//   // add dynamic http resources
+//   SERVER.on("/fs", HTTP_GET, [](AsyncWebServerRequest *request) {
+//     SERVER.send(request, FILESYSTEM.getStorageDetails());
+//   });
+//   SERVER.on("/files", HTTP_GET, [](AsyncWebServerRequest *request) {
+//     SERVER.send(request, FILESYSTEM.getFileListing());
+//   });
+//   SERVER.on("/sta", HTTP_GET, [](AsyncWebServerRequest *request) {
+//     SERVER.send(request, WIFI_STA_CFG.getDetails());
+//   });
+//   SERVER.on("/ap", HTTP_GET, [](AsyncWebServerRequest *request) {
+//     SERVER.send(request, WIFI_AP_CFG.getDetails());
+//   });
+//   SERVER.on("/sensor", HTTP_GET, [](AsyncWebServerRequest *request) {
+//     SERVER.send(request, getLastSensorValues());
+//   });
+//   SERVER.on("/bmp280", HTTP_GET, [](AsyncWebServerRequest *request) {
+//     SERVER.send(request, _bmp280.getJsonValue());
+//   });
+//   SERVER.on("/dht22", HTTP_GET, [](AsyncWebServerRequest *request) {
+//     SERVER.send(request, _dht22.getJsonValue());
+//   });
+//   SERVER.on("/mq135", HTTP_GET, [](AsyncWebServerRequest *request) {
+//     SERVER.send(request, _mq135.getJsonValue());
+//   });
+
+  LOG.verbose(F("========================="));
+  LOG.verbose(F("Setup finished. Have fun."));
+  LOG.verbose(F("========================="));
+}
+
+void write(const char *name, JsonObject &json) {
+  
+  LOG.verbose(F("%s|%s"), name, esp8266util::toString(json).c_str());
+  Firebase.set(name, json);
+  if (Firebase.failed()) {
+    LOG.error(F("Saving %s values to Firebase failed: Reason: %s"), name, Firebase.error().c_str());
+  }
 }
 
 void loop() {
-  esp8266->run();
-  // time for RTOS functions
-  delay(20);
+  
+  if (SYS_CFG.nextLoopInterval()) {
+    MDNS.update();
+
+    _bmp280.update(true);
+    _dht22.update(true);
+    _mq135.update(true);
+    write("BMP280", _bmp280.getJsonValue());
+    write("DHT22", _dht22.getJsonValue());
+    write("MQ135", _mq135.getJsonValue());
+  }
 }
