e90593721af1f6740e485847de934465752e1218
hunsalz <info@hunsalz.de>
2018-11-19 15:28:01 +0100

    initial Parse server version

---
+++ Parse-Server/Parse-Server.ino
@@ -0,0 +1,170 @@
+#include <Esp8266Utils.h>     // https://github.com/hunsalz/esp8266utils
+#include <Parse.h>            // https://github.com/parse-community/Parse-SDK-Arduino
+#include <Log4Esp.h>          // https://github.com/hunsalz/log4Esp
+
+#include "config.h"
+
+// web server settings
+const static int PORT = 80;
+
+esp8266utils::BME280Sensor _bme280;
+//esp8266utils::BMP280Sensor _bmp280;
+//esp8266utils::DHTSensor _dht11;
+//esp8266utils::DHTSensor _dht22;
+//esp8266utils::MQ135Sensor _mq135;
+
+void setup() {
+
+  // logger setup
+  LOG.addLevelToAll(Appender::VERBOSE);
+  LOG.addFormatterToAll(
+      [](Print &output, Appender::Level level, const char *msg, va_list *args) {
+        // output log level
+        output.print(Appender::toString(level, true));
+        output.print(Appender::DEFAULT_SEPARATOR);
+        // output uptime of this program in milliseconds
+        output.print(millis());
+        output.print(Appender::DEFAULT_SEPARATOR);
+        // output free heap space
+        output.print(ESP.getFreeHeap());
+        output.print(Appender::DEFAULT_SEPARATOR);
+        // determine buffer length for formatted data
+        size_t length = vsnprintf(NULL, 0, msg, *args) + 1;
+        char buffer[length];
+        // output formatted data
+        vsnprintf(buffer, length, msg, *args);
+        output.print(buffer);
+      });
+
+  // serial setup
+  Serial.begin(115200);
+  Serial.setDebugOutput(false);
+  while (!Serial && !Serial.available()) {
+  };
+  Serial.println();
+  LOG.verbose(F("Serial baud rate is [%d]"), Serial.baudRate());
+
+  // sensor setup
+  if (_bme280.begin(0x76)) {
+    LOG.verbose(F("BME280 is ready."));
+  } else {
+    LOG.error(F("Setup BME280 failed!"));
+  }
+  //_dht11.begin(4, 11); // 2, 3, 5, 7
+  //_dht22.begin(2, 22);
+  //_mq135.begin(A0);
+
+  // WiFi setup
+  WIFI_STA_CFG.addAP(WIFI_SSID_1, WIFI_PSK_1);
+  WIFI_STA_CFG.addAP(WIFI_SSID_2, WIFI_PSK_2);
+  WIFI_STA_CFG.begin();
+
+  // Parse setup
+  ParseClient pc = ParseClient();
+  pc.begin(APPLICATION_ID, CLIENT_KEY);
+
+  // increase loop interval
+  SYS_CFG.setLoopInterval(LOOP_INTERVAL);
+
+  #ifdef ENABLE_WEBSERVER
+
+  // MDNS setup
+  MDNS_CFG.begin("esp8266");
+  MDNS.addService("http", "tcp", PORT);
+
+  // file system setup to enable static web server content
+  FILESYSTEM.begin();
+
+  // web server setup
+  SERVER.begin();
+  // rewrite root context
+  SERVER.getWebServer().rewrite("/", "/index.build.html");
+  // handle static web resources
+  SERVER.getWebServer().serveStatic("/", SPIFFS, "/www/", "max-age:15");
+  // cache-control 15 seconds
+  // add dynamic http resources
+  SERVER.on("/fs", HTTP_GET, [](AsyncWebServerRequest* request) {
+    SERVER.send(request, esp8266utils::APP_JSON, FILESYSTEM.getStorageDetails());
+  });
+  SERVER.on("/files", HTTP_GET, [](AsyncWebServerRequest* request) {
+    SERVER.send(request, esp8266utils::APP_JSON, FILESYSTEM.getFileListing());
+  });
+  SERVER.on("/sta", HTTP_GET, [](AsyncWebServerRequest* request) {
+    SERVER.send(request, esp8266utils::APP_JSON, WIFI_STA_CFG.getDetails());
+  });
+  SERVER.on("/esp", HTTP_GET, [](AsyncWebServerRequest* request) {
+    SERVER.send(request, esp8266utils::APP_JSON, SYS_CFG.getDetails());
+  });
+  SERVER.on("/bme280", HTTP_GET, [](AsyncWebServerRequest* request) {
+    SERVER.send(request, esp8266utils::APP_JSON, _bme280.getValuesAsJson());
+  });
+  SERVER.on("/dht11", HTTP_GET, [](AsyncWebServerRequest* request) {
+    //SERVER.send(request, esp8266utils::APP_JSON, _dht11.getValuesAsJson());
+  });
+  SERVER.on("/dht22", HTTP_GET, [](AsyncWebServerRequest* request) {
+    //SERVER.send(request, esp8266utils::APP_JSON, _dht22.getValuesAsJson());
+  });
+  SERVER.on("/mq135", HTTP_GET, [](AsyncWebServerRequest* request) {
+    //SERVER.send(request, esp8266utils::APP_JSON, _mq135.getValuesAsJson());
+  });
+
+  #endif // NO_WEBSERVER
+
+  // save current ESP settings to Firebase
+  set("esp", SYS_CFG.getDetails());
+
+  LOG.verbose(F("========================="));
+  LOG.verbose(F("Setup finished. Have fun."));
+  LOG.verbose(F("========================="));
+}
+
+void set(const char *name, String json) {
+  
+  LOG.verbose(F("Set value|%s|%s"), name, json.c_str());
+
+
+
+  // Firebase.setRawJson(name, json);
+  // if (Firebase.failed()) {
+  //   LOG.error(F("Saving %s value to Firebase failed: Reason: %s"), name, Firebase.error().c_str());
+  // }
+}
+
+void push(const char *name, String json) {
+  
+  LOG.verbose(F("Push value|%s|%s"), name, json.c_str());
+
+
+
+  // Firebase.pushRawJson(name, json);
+  // if (Firebase.failed()) {
+  //   LOG.error(F("Saving %s value to Firebase failed: Reason: %s"), name, Firebase.error().c_str());
+  // }
+}
+
+void loop() {
+  
+  ESP.wdtDisable();
+  if (SYS_CFG.nextLoopInterval()) {
+       
+    #ifdef ENABLE_WEBSERVER
+    MDNS.update();
+    #endif // NO_WEBSERVER
+
+    _bme280.update(USE_MOCK_DATA);
+    //_dht11.update();
+    //_dht22.update(USE_MOCK_DATA);
+    //_mq135.update(USE_MOCK_DATA);
+
+    // push sensor values to Firebase
+    push("bme280", _bme280.getValuesAsJson());
+    //push("dht11", _dht11.getValuesAsJson());
+    //push("dht22", _dht22.getValuesAsJson());
+    //push("mq135", _mq135.getValuesAsJson());
+  }
+  ESP.wdtEnable(30000);
+
+  //ESP.deepSleep(30e6);
+  // reserve time for core processes
+  delay(500);
+}
