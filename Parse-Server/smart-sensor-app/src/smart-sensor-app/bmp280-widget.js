b89321f4d65109f28b765ecae0213fb93af7c91e
hunsalz <info@hunsalz.de>
2018-11-19 13:42:48 +0100

    upgrade to Polymer 3.0; moved app from Firebase to Parse-Server

---
+++ Parse-Server/smart-sensor-app/src/smart-sensor-app/bmp280-widget.js
@@ -0,0 +1,239 @@
+import {PolymerElement, html} from '@polymer/polymer/polymer-element.js';
+import {mixinBehaviors} from '@polymer/polymer/lib/legacy/class.js';
+import {IronResizableBehavior} from '@polymer/iron-resizable-behavior/iron-resizable-behavior.js';
+import {afterNextRender} from '@polymer/polymer/lib/utils/render-status.js';
+
+import '@polymer/paper-styles/paper-styles.js';
+
+import 'parse/dist/parse.min.js';
+import 'chart.js/dist/Chart.bundle.min.js';
+
+class Bmp280Widget extends mixinBehaviors([IronResizableBehavior], PolymerElement) {
+  static get template() {
+    return html`
+      <style> 
+        :host {
+          display: block;
+        }
+
+        .container {
+          margin-bottom: 4vh;
+        }
+
+        .subtitle {
+          text-align: center;
+          color: var(--paper-blue-grey-700);
+          font-size: 0.8em;
+          @apply --layout-flex;
+          width: 100%;
+        }
+      </style>
+
+      <div class="container">
+        <div class="subtitle">Last temperature measured on
+          <b>[[lastUpdate]]</b> is
+          <b>[[lastTemperature]]°</b>
+        </div>
+        <canvas id="chart1" aria-label="Temperature chart" role="chart"></canvas>
+      </div>
+      <div class="container">
+        <div class="subtitle">Last pressure measured on
+          <b>[[lastUpdate]]</b> is
+          <b>[[lastPressure]] P</b>
+        </div>
+        <canvas id="chart2" aria-label="Pressure chart" role="chart"></canvas>
+      </div>
+      <div class="container">
+        <div class="subtitle">Last altitude measured on
+          <b>[[lastUpdate]]</b> is
+          <b>[[lastAltitude]] m</b>
+        </div>
+        <canvas id="chart3" aria-label="Altitude chart" role="chart"></canvas>
+      </div>
+    `;
+  }
+
+  static get properties() {
+    return {
+      temperatures: {
+        type: Object,
+        notify: true
+      },
+      lastTemperature: {
+        type: String,
+        value: 'n.a.',
+        notify: true
+      },
+      pressures: {
+        type: Array,
+        notify: true
+      },
+      lastPressure: {
+        type: String,
+        value: 'n.a.',
+        notify: true
+      },
+      altitudes: {
+        type: Array,
+        notify: true
+      },
+      lastAltitude: {
+        type: String,
+        value: 'n.a.',
+        notify: true
+      },
+      lastUpdate: {
+        type: String,
+        notify: true
+      }
+    };
+  }
+
+  constructor() {
+    super();
+   
+    this.__queryBMP280Entries();
+
+    afterNextRender(this, function () {
+      // global chart properties
+      Chart.defaults.global.legend.display = false; // disable chart legend   
+      // initialize temperature chart
+      var ctx = this.$.chart1.getContext('2d');
+      this.temperatures = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: [],
+          datasets: [{
+            label: 'Temperature',
+            borderColor: '#e57373',
+            borderWidth: 1,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            data: []
+          }]
+        }
+      });
+      // initialize pressure chart
+      var ctx = this.$.chart2.getContext('2d');
+      this.pressures = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: [],
+          datasets: [{
+            label: 'Pressure',
+            borderColor: '#e57373',
+            borderWidth: 1,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            data: []
+          }]
+        }
+      });
+      // initialize altitude chart
+      var ctx = this.$.chart3.getContext('2d');
+      this.altitudes = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: [],
+          datasets: [{
+            label: 'Altitude',
+            borderColor: '#e57373',
+            borderWidth: 1,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            data: []
+          }]
+        }
+      });
+      // handle resize event
+      this.addEventListener('iron-resize', () => {
+        this.temperatures.resize();
+      });
+    });
+  }
+
+  async __queryBMP280Entries(limit = 5) {
+    
+    // query BMP280 entries
+    const BMP280 = Parse.Object.extend('BMP280');
+    const query = new Parse.Query(BMP280);
+    query.descending("createdAt");
+    query.limit(limit);
+
+    // initially query all entries to draw chart once
+    query.find().then((results) => {
+      if (results.length > 0) {
+        // add each entry
+        results.forEach(e => {
+          let label = __this.__getShortTime(e.get('createdAt'));
+          // update temperature chart
+          this.temperatures.data.labels.push(label);
+          this.temperatures.data.datasets[0].data.push(e.get('temperature'));
+          // update pressure chart
+          this.pressures.data.labels.push(label);
+          this.pressures.data.datasets[0].data.push(e.get('pressure'));
+          // update altitude chart
+          this.altitudes.data.labels.push(label);
+          this.altitudes.data.datasets[0].data.push(e.get('altitude'));
+        });
+        // update charts
+        this.temperatures.update();
+        this.pressures.update();
+        this.altitudes.update();
+        // update last update date
+        this.lastUpdate = __this.__getShortDate(results[0].get('createdAt'));
+        // update last temperature value
+        this.lastTemperature = results[0].get('temperature');
+        // update last pressure value
+        this.lastPressure = results[0].get('pressure');
+        // update last altitude value
+        this.lastAltitude = results[0].get('altitude');
+      }
+    }, (error) => {
+      console.error("Query BM280 entries failed.", error);
+    });
+
+    // subscribe to get updates
+    var subscription = query.subscribe();
+    // handle incoming event
+    var __this = this;
+    subscription.on('create', function(bmp280) {
+      // add new entry and drop the oldest one
+      let label = __this.__getShortTime(bmp280.get('createdAt'));
+      // update temperature chart
+      __this.temperatures.data.labels.unshift(label);
+      __this.temperatures.data.labels.pop();
+      __this.temperatures.data.datasets[0].data.unshift(bmp280.get('temperature'));
+      __this.temperatures.data.datasets[0].data.pop();
+      // update pressure chart
+      __this.pressures.data.labels.unshift(label);
+      __this.pressures.data.labels.pop();
+      __this.pressures.data.datasets[0].data.unshift(bmp280.get('pressure'));
+      __this.pressures.data.datasets[0].data.pop();
+      // update pressure chart
+      __this.altitudes.data.labels.unshift(label);
+      __this.altitudes.data.labels.pop();
+      __this.altitudes.data.datasets[0].data.unshift(bmp280.get('pressure'));
+      __this.altitudes.data.datasets[0].data.pop();
+      // update charts
+      __this.temperatures.update();
+      __this.pressures.update();
+      __this.altitudes.update();
+      // update last update date
+      __this.lastUpdate = __this.__getShortDate(bmp280.get('createdAt'));
+      // update last temperature value
+      __this.lastTemperature = bmp280.get('temperature');
+      // update last pressure value
+      __this.lastPressure = bmp280.get('pressure');
+      // update last altitude value
+       __this.lastAltitude = bmp280.get('altitude');
+    });
+  }
+
+  __getShortDate(date) {
+    return date.toDateString().substring(0, 15);
+  }
+
+  __getShortTime(date) {
+    return date.toTimeString().substring(0, 8);
+  }
+}
+
+window.customElements.define('bmp280-widget', Bmp280Widget);
---
+++ Parse-Server/smart-sensor-app/src/smart-sensor-app/widget-layout.js
@@ -0,0 +1,107 @@
+import {PolymerElement, html} from '@polymer/polymer/polymer-element.js';
+import {GestureEventListeners} from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
+
+import '@polymer/app-storage/app-localstorage/app-localstorage-document.js';
+
+import '@polymer/iron-collapse/iron-collapse.js';
+import '@polymer/iron-flex-layout/iron-flex-layout.js';
+
+import '@polymer/paper-styles/paper-styles.js';
+
+class WidgetLayout extends GestureEventListeners(PolymerElement) {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: inline-block;
+        }
+
+        .widget-header {
+          background-color: white;
+          border: 1px solid var(--paper-grey-100);
+          border-top-left-radius: 5px;
+          border-top-right-radius: 5px;
+          font-size: 1em;
+          color: var(--paper-blue-grey-700);
+          font-weight: normal;
+          cursor: pointer;
+          width: 100%;
+        }
+
+        .header {
+          padding: 10px;
+          @apply --layout-horizontal;
+          @apply --layout-center;
+        }
+
+        iron-collapse {
+          padding: 10px;
+          border: 1px solid var(--paper-grey-100);
+          border-top: none;
+          border-bottom-left-radius: 5px;
+          border-bottom-right-radius: 5px;
+          @apply --shadow-elevation-2dp;
+          height: 100%;
+        }
+
+        .spacer {
+          @apply --layout-flex;
+          width: 100%;
+        }
+      </style>
+
+      <app-localstorage-document key="[[key]]" data="{{opened}}" log$={{verbose}}></app-localstorage-document>
+
+      <div class="widget-header" on-tap="toggle">
+        <div class="header">
+          <slot name="title"></slot>
+          <span>[[title]]</span>
+          <span class="spacer"></span>
+          <slot name="label"></slot>
+        </div>
+      </div>
+      <iron-collapse id="collapse_id" opened="{{opened}}" tabindex="0">
+        <slot></slot>
+      </iron-collapse>
+    `;
+  }
+
+  static get properties() {
+    return {
+      title: {
+        type: String
+      },
+      key: {
+        type: String
+      },
+      verbose: {
+        type: Boolean,
+        value: false
+      }
+    };
+  }
+
+  /**
+   * true if widget is collapsed
+   */
+  isCollapsed() {
+    return !this.$.collapse_id.opened;
+  }
+
+  /**
+   * collapses widget more or less 
+   */
+  collapse(collapse) {
+    this.$.collapse_id.opened = !collapse;
+  }
+
+  /**
+   * toggle between collapsed or expanded widget
+   */
+  toggle() {
+    this.$.collapse_id.toggle();
+    this.$.collapse_id.notifyResize();
+  }
+}
+
+window.customElements.define('widget-layout', WidgetLayout);
