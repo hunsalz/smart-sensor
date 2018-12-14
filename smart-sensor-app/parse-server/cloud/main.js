03f03e59d1a95dd7a0e5a6c1f2a894e168738da2
hunsalz <info@hunsalz.de>
2018-12-14 18:25:32 +0100

    limit history by device identifier

--- smart-sensor-app/parse-server/cloud/main.js
+++ smart-sensor-app/parse-server/cloud/main.js
@@ -1,10 +1,11 @@
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
-});
-
 Parse.Cloud.afterSave("BMP280", (request) => {
+  
   const query = new Parse.Query("BMP280");
   query.descending("createdAt");
+  // narrow by device if given
+  if (request.device) {
+    query.equalTo("device", request.device);
+  }
   query.skip(100); // keep the last 100 entries
   query.find({sessionToken: request.user.getSessionToken()})
     .then((results) => {
@@ -17,8 +18,13 @@ Parse.Cloud.afterSave("BMP280", (request) => {
 });
 
 Parse.Cloud.afterSave("BME280", (request) => {
+  
   const query = new Parse.Query("BME280");
   query.descending("createdAt");
+  // narrow by device if given
+  if (request.device) {
+    query.equalTo("device", request.device);
+  }
   query.skip(100); // keep the last 100 entries
   query.find({sessionToken: request.user.getSessionToken()})
     .then((results) => {
@@ -31,13 +37,18 @@ Parse.Cloud.afterSave("BME280", (request) => {
 });
 
 Parse.Cloud.afterSave("BME680", (request) => {
+  
   const query = new Parse.Query("BME680");
   query.descending("createdAt");
+  // narrow by device if given
+  if (request.device) {
+    query.equalTo("device", request.device);
+  }
   query.skip(100); // keep the last 100 entries
-  query.find()
+  query.find({sessionToken: request.user.getSessionToken()})
     .then((results) => {
       results.forEach(e => {
-        e.destroy();
+        e.destroy({sessionToken: user.getSessionToken()});
       });
     }, (error) => {
       console.error("Query BME680 entries failed.", error);
