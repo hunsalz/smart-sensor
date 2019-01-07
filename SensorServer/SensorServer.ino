0d6b58162539091a81bc23a5c55b542b1474bedf
hunsalz <info@hunsalz.de>
2019-01-07 22:53:50 +0100

    reflected esp8286utils changes

--- SensorServer/SensorServer.ino
+++ SensorServer/SensorServer.ino
@@ -1,4 +1,4 @@
-#include <ESP8266HTTPClient.h>  // https://github.com/esp8266/Arduino
+#include <ESP8266mDNS.h>        // https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266mDNS/src/ESP8266mDNS.h
 #include <ESP8266WiFiMulti.h>   // https://github.com/esp8266/Arduino/tree/master/libraries/ESP8266WiFi/src/ESP8266WiFiMulti.h
 #include <StreamString.h>       // https://github.com/esp8266/Arduino/blob/master/cores/esp8266/StreamString.h
 
@@ -8,7 +8,9 @@
 
 esp8266utils::BME280Sensor bme280;
 
-unsigned long next = 0;
+unsigned long nextLoopInterval = 0;
+
+StreamString* payload;
 
 void setup() {
 
@@ -22,29 +24,35 @@ void setup() {
   wifiMulti.addAP(WIFI_SSID_2, WIFI_PSK_2);
   esp8266utils::setupWiFiSta(wifiMulti);
 
+  // MDNS setup
+  const char* hostname = "esp8266";
+  if (MDNS.begin(hostname)) {
+    // add service to MDNS-SD
+    MDNS.addService("http", "tcp", 80);
+    VERBOSE_FP(F("MDNS enabled to http://%s.local"), hostname);
+  } else {
+     ERROR_FP(F("MDNS failed for http://%s.local"), hostname);
+  }
+
   // sensor setup
   if (bme280.begin(0x76)) {
-    VERBOSE_FP(F("BME280 is ready for %s"), bme280.getDeviceName());
+    VERBOSE_FP(F("BME280 is ready."));
   } else {
     ERROR_FP(F("Setup BME280 failed!"));
   }
-
-  // MDNS setup
-  //MDNS_CFG.begin("esp8266");
-  //MDNS.addService("http", "tcp", PORT);
 
   // file system setup to enable static web server content
   esp8266utils::FileSystem fs; 
   fs.begin();
 
-  // web server setup
+  // general web server setup
   esp8266utils::WebService webService(80);
   webService.begin();
   // rewrite root context
   webService.getWebServer().rewrite("/", "/index.html");
   // handle static web resources
   webService.getWebServer().serveStatic("/", SPIFFS, "/www/", "max-age:15");
-  // cache-control 15 seconds
+  
   // add dynamic http resources
   webService.on("/fs", HTTP_GET, [&fs](AsyncWebServerRequest* request) {
 
@@ -53,6 +61,7 @@ void setup() {
     size_t size = fs.serializeInfo(*payload);
     response->print(*payload); 
     request->send(response);
+    VERBOSE(*payload);
     delete payload;
   });
   webService.on("/files", HTTP_GET, [&fs](AsyncWebServerRequest* request) {
@@ -62,6 +71,7 @@ void setup() {
     size_t size = fs.serializeListing(*payload);
     response->print(*payload); 
     request->send(response);
+    VERBOSE(*payload);
     delete payload;
   });
   webService.on("/sta", HTTP_GET, [](AsyncWebServerRequest* request) {
@@ -71,6 +81,7 @@ void setup() {
     size_t size = esp8266utils::serializeWiFiSta(*payload);
     response->print(*payload); 
     request->send(response);
+    VERBOSE(*payload);
     delete payload;
   });
   webService.on("/esp", HTTP_GET, [](AsyncWebServerRequest* request) {
@@ -80,6 +91,7 @@ void setup() {
     size_t size = esp8266utils::serializeESP(*payload);
     response->print(*payload); 
     request->send(response);
+    VERBOSE(*payload);
     delete payload;
   });
   webService.on("/bme280", HTTP_GET, [](AsyncWebServerRequest* request) {
@@ -89,6 +101,7 @@ void setup() {
     size_t size = bme280.serialize(*payload);
     response->print(*payload); 
     request->send(response);
+    VERBOSE(*payload);
     delete payload;
   });
 
@@ -97,15 +110,12 @@ void setup() {
   VERBOSE_FP(F("========================="));
 }
 
-StreamString* payload;
-
 void loop() {
 
-  if (millis() > next) {
+  if (millis() > nextLoopInterval) {  
+    nextLoopInterval = millis() + 5000;
     
-    next = millis() + 5000;
-    
-    //MDNS.update();
+    MDNS.update();
 
     // read sensor values
     bme280.update(USE_MOCK_DATA);
