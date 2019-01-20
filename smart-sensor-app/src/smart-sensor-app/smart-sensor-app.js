a01f559216710b7bdacef736aac8fe6ea7608150
hunsalz <info@hunsalz.de>
2019-01-21 00:13:53 +0100

    removed ticks, but settings-page still available

--- smart-sensor-app/src/smart-sensor-app/smart-sensor-app.js
+++ smart-sensor-app/src/smart-sensor-app/smart-sensor-app.js
@@ -100,8 +100,8 @@ class SmartSensorApp extends GestureEventListeners(PolymerElement) {
 
         <div class="content-area">
           <iron-pages attr-for-selected="id" selected="{{routeData.page}}" fallback-selection="dashboard">
-            <widgets-page id="dashboard" collapsed="{{collapsed}}" ticks={{ticks}}></widgets-page>
-            <settings-page id="settings" ticks={{ticks}}></settings-page>
+            <widgets-page id="dashboard" collapsed="{{collapsed}}"></widgets-page>
+            <settings-page id="settings"></settings-page>
           </iron-pages>
         </div>
 
