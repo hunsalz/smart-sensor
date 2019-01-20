137ec66ab32cb91935df40c5d589adc834ee86a0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-20 11:21:25 +0000

    Deploy hunsalz/smart-sensor to github.com/hunsalz/smart-sensor.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","90f2c39e69c47ac2b6dd8499ba0629bd"],["src/smart-sensor-app/main-app.js","17197829d35aad7ae843801b4d72d2fd"]];
+var precacheConfig = [["index.html","f265cc633735b47a43e6332b1ab87ad8"],["src/smart-sensor-app/main-app.js","17197829d35aad7ae843801b4d72d2fd"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
