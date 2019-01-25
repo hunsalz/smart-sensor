b465e0564d4acf1a7593f418054c4ba92fcc820f
hunsalz <info@hunsalz.de>
2019-01-25 15:40:10 +0100

    use appropriate namespace

--- smart-sensor-esp/smart-sensor-esp.ino
+++ smart-sensor-esp/smart-sensor-esp.ino
@@ -3,11 +3,11 @@
 #include <ESP8266WiFiMulti.h>   // https://github.com/esp8266/Arduino/tree/master/libraries/ESP8266WiFi/src/ESP8266WiFiMulti.h
 #include <StreamString.h>       // https://github.com/esp8266/Arduino/blob/master/cores/esp8266/StreamString.h
 
-#include <ESPUtils.h>           // https://github.com/hunsalz/ESPUtils
+#include <EspUtils.h>           // https://github.com/hunsalz/EspUtils
 
 #include "config.h"
 
-using namespace ESPUtils;
+using namespace espUtils;
 
 ESP8266WebServer server(80);
 BME280Sensor bme280;
