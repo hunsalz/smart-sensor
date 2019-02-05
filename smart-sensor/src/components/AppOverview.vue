51d9237f6a8777847a800c1a6f72ac3d4bbde157
hunsalz <info@hunsalz.de>
2019-02-05 14:52:14 +0100

    added grid layout settings

--- smart-sensor/src/components/AppOverview.vue
+++ smart-sensor/src/components/AppOverview.vue
@@ -1,14 +1,17 @@
 <template>
-  <v-container fluid grid-list-sm>
-    <v-layout row wrap>
-      <v-flex v-for="i in 12" :key="i" xs4>
-        <img
-          :src="`https://randomuser.me/api/portraits/women/${i + 20}.jpg`"
-          class="image"
-          alt="lorem"
-          width="100%"
-          height="100%"
-        >
+  <v-container grid-list-md>
+    <v-layout align-space-between justify-start wrap>
+      <v-flex v-for="i in 5" :key="i" xs6>
+        <v-expansion-panel expand>
+          <v-expansion-panel-content>
+            <div slot="header">ESP-8266</div>
+            <v-card>
+              <v-card-text
+                class="grey lighten-3"
+              >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-card-text>
+            </v-card>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
       </v-flex>
     </v-layout>
   </v-container>
