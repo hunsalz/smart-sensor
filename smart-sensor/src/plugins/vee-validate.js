2a9dc012e530d27a6271f4006a92c77643b92e56
hunsalz <info@hunsalz.de>
2019-02-08 12:43:21 +0100

    temporary adapted locale

--- smart-sensor/src/plugins/vee-validate.js
+++ smart-sensor/src/plugins/vee-validate.js
@@ -1,9 +1,22 @@
-import Vue from 'vue';
-import VeeValidate from 'vee-validate';
-import VueI18n from 'vue-i18n';
+import Vue from 'vue'
+import VeeValidate, { Validator } from 'vee-validate'
+//import messagesEN from 'vee-validate/dist/locale/en'
+import messagesDE from 'vee-validate/dist/locale/de'
+//import VueI18n from 'vue-i18n'
 
-const i18n = new VueI18n();
+// const i18n = new VueI18n();
 
-Vue.use(VeeValidate, {
-    validity: true
-});
+// Vue.use(VeeValidate, {
+//     i18nRootKey: 'validations',
+//     i18n,
+//     dictionary: {
+//         en: { messages: { ...messagesEN } },
+//         de: { messages: { ...messagesDE } },
+//     }
+// });
+
+// TODO https://baianat.github.io/vee-validate/guide/localization.html#vuei18n-integration
+
+Validator.localize({ de: messagesDE })
+
+Vue.use(VeeValidate, {locale: 'de'})
