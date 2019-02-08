61643bd507cec0fbdb8e6a9585dc0f3db89af1f5
hunsalz <info@hunsalz.de>
2019-02-08 22:56:45 +0100

    added store functionality

--- smart-sensor/src/components/PanelHeader.vue
+++ smart-sensor/src/components/PanelHeader.vue
@@ -3,14 +3,14 @@
     <v-layout row>
       <v-flex>
         <v-layout column>
-          <v-flex>Label</v-flex>
-          <v-flex>ESP-0000001</v-flex>
+          <v-flex>{{ label }}</v-flex>
+          <v-flex>{{ device }}</v-flex>
         </v-layout>
       </v-flex>
       <v-spacer />
       <v-flex>
         <v-layout column>
-          <v-flex>{{ $t('label.temperature') }} 22 °</v-flex>
+          <v-flex>{{ $t('label.temperature') }} {{ getTemperature }} °</v-flex>
           <v-flex>{{ $t('label.humidity') }} 54 %</v-flex>
           <v-flex>{{ $t('label.pressure') }} 32322 Pa</v-flex>
           <v-flex>{{ $t('label.altitude') }} 122 m</v-flex>
@@ -21,9 +21,25 @@
 </template>
 
 <script>
-export default {
-  name: 'PanelHeader'
-}
+  export default {
+    name: "PanelHeader",
+    data: function() {
+      return {
+        label: "MeinSensor",
+        device: "ESP-000023f09d"
+      };
+    },
+    computed: {
+      getTemperature() {
+        return this.$store.state.temperature;
+      }
+    },
+    created() {
+      this.$store.dispatch("first", {
+        device: this.device
+      });
+    }
+  };
 </script>
 
 <style scoped>
