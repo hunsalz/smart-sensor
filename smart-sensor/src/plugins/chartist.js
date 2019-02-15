825043f057ec30093a84bcfe5e76f31bf776f213
hunsalz <info@hunsalz.de>
2019-02-16 00:38:06 +0100

    added chartist.js

--- smart-sensor/src/plugins/chartist.js
+++ smart-sensor/src/plugins/chartist.js
@@ -1,4 +1,4 @@
 import Vue from 'vue'
 import 'chartist/dist/chartist.min.css'
 
-Vue.use(require('vue-chartist'))
+Vue.use(require('vue-chartist'), { messageNoData: 'You have not enough data', classNoData: 'empty' })
