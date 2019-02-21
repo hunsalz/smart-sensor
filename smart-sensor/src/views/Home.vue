7b44968428547236b5766f570cab5a0f51076486
hunsalz <info@hunsalz.de>
2019-02-21 17:41:26 +0100

    added extended tab configuration

--- smart-sensor/src/views/Home.vue
+++ smart-sensor/src/views/Home.vue
@@ -20,9 +20,25 @@
                 :label="device.label"
               />
             </div>
-            <panel-tabs>
-              <panel-chart :device="device.name" />
-            </panel-tabs>
+            <v-tabs grow>
+              <v-tab
+                v-for="(tab, index1) in tabs"
+                :key="index1"
+                class="caption"
+              >
+                {{ $t(tab.name) }}
+              </v-tab>
+              <v-tab-item
+                v-for="(tab, index2) in tabs"
+                :key="index2"
+                lazy
+              >
+                <panel-chart
+                  :device="device.name"
+                  :filter="tabs[index2]"
+                />
+              </v-tab-item>
+            </v-tabs>
           </v-expansion-panel-content>
         </v-expansion-panel>
       </v-flex>
@@ -32,15 +48,42 @@
 
 <script>
   import PanelHeader from "@/components/PanelHeader";
-  import PanelTabs from "@/components/PanelTabs";
   import PanelChart from "@/components/PanelChart";
 
   export default {
     name: "Home",
     components: {
       PanelHeader,
-      PanelTabs,
       PanelChart
+    },
+    data: function() {
+      return {
+        tabs: [
+          {
+            name: "tabs.last10",
+            key: "LAST_10",
+            limit: 10
+          },
+          {
+            name: "tabs.last4hours",
+            key: "LAST_4_HOURS",
+            offsetFromNowInMillis: 4 * 60 * 60 * 1000,
+            limit: 1000
+          },
+          {
+            name: "tabs.last24hours",
+            key: "LAST_24_HOURS",
+            offsetFromNowInMillis: 24 * 60 * 60 * 1000,
+            limit: 1000
+          },
+          {
+            name: "tabs.last7days",
+            key: "LAST_7_DAYS",
+            offsetFromNowInMillis: 7 * 24 * 60 * 60 * 1000,
+            limit: 1000
+          }
+        ]
+      };
     },
     computed: {
       devices: function() {
