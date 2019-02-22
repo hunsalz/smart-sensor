f827143c2856851b6efcef82f8dfe8ab75715342
hunsalz <info@hunsalz.de>
2019-02-22 14:43:10 +0100

    added pwa config

--- smart-sensor-app/vue.config.js
+++ smart-sensor-app/vue.config.js
@@ -1,11 +1,20 @@
 // eslint-disable-next-line no-undef
 module.exports = {
+  publicPath: '/',
+  lintOnSave: process.env.NODE_ENV !== 'production',
   pluginOptions: {
     i18n: {
       locale: 'en',
       fallbackLocale: 'en',
       localeDir: 'locales',
       enableInSFC: true
+    },
+    pwa: {
+      name: 'Smart Sensor',
+      themeColor: '#1976D2',
+      msTileColor: '#000000',
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: 'default'
     }
   }
 }
