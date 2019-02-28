d65237b6c9344314699a4dedb0512c7318d18c13
hunsalz <info@hunsalz.de>
2019-03-01 00:17:43 +0100

    added basic functionality

--- smart-sensor-app/src/views/More.vue
+++ smart-sensor-app/src/views/More.vue
@@ -5,17 +5,58 @@
       justify-center
     >
       <v-flex>
-        <div>
-          More coming soon ...
-        </div>
+        <v-card class="elevation-12">
+          <v-toolbar color="primary">
+            <v-toolbar-title class="white--text">
+              {{ $t('app.more.name') }}
+            </v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                v-for="(device, index) in devices"
+                :key="index"
+                prepend-icon="label"
+                name="label"
+                type="text"
+                counter
+                maxlength="20"
+                :label="device.name"
+                :value="device.label"
+              />
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn
+              color="primary"
+              @click="submit"
+            >
+              {{ $t('app.more.btn') }}
+            </v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+  import { MODULES } from "@/store";
+
   export default {
-    name: "More"
+    name: "More",
+    computed: {
+      devices: function() {
+        // note: should be already available while view 'Home' depends on it
+        return this.$store.getters[MODULES.Device.getters.getDevices];
+      }
+    },
+    methods: {
+      submit() {
+        // TODO
+      }
+    }
   };
 </script>
 
