3d02e45bf209ef890db269eb0019a7a5928241d5
hunsalz <info@hunsalz.de>
2019-03-04 12:49:39 +0100

    use .env to define locale

--- smart-sensor-app/src/plugins/moment.js
+++ smart-sensor-app/src/plugins/moment.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
 
 const moment = require('moment')
-require('moment/locale/de')
+require('moment/locale/' + process.env.VUE_APP_I18N_LOCALE)
 
 Vue.use(require('vue-moment'), {
   moment
 })
-
-// TODO finalize i18n
-// console.log(Vue.moment().locale())
