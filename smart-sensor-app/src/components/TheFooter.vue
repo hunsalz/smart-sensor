cdb4c37144b195e677ce3666938b5cc9945f6744
hunsalz <info@hunsalz.de>
2019-03-11 15:34:18 +0100

    initial version

---
+++ smart-sensor-app/src/components/TheFooter.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-footer height="auto" color="primary" fixed>
+    <v-layout justify-center row>
+      <v-btn
+        flat
+        href="https://github.com/hunsalz/smart-sensor"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <span class="white--text">
+          GitHub
+        </span>
+      </v-btn>
+    </v-layout>
+  </v-footer>
+</template>
+
+<script>
+import router from "@/router";
+import { MODULES } from "@/store";
+
+export default {
+  name: "TheFooter",
+  computed: {
+    isAuthenticated() {
+      return this.$store.getters[MODULES.User.getters.isAuthenticated];
+    }
+  },
+  methods: {
+    home() {
+      router.push({ name: "home" });
+    },
+    logout() {
+      this.$store.dispatch(MODULES.User.actions.logout);
+    },
+    more() {
+      router.push({ name: "more" });
+    }
+  }
+};
+</script>
+
+<style scoped></style>
