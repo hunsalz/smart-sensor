5fa105fff07ba33ddec363d74340ec4be4cf1af2
hunsalz <info@hunsalz.de>
2019-03-14 18:11:15 +0100

    added expand button

--- smart-sensor-app/src/components/TheNavigation.vue
+++ smart-sensor-app/src/components/TheNavigation.vue
@@ -7,11 +7,12 @@
       </span>
     </v-toolbar-title>
     <v-spacer />
-    <div v-if="isAuthenticated">
-      <v-btn flat @click="logout">
-        {{ $t("app.logout.name") }}
-      </v-btn>
-    </div>
+    <!--v-btn v-show="$route.name !== 'login'" flat @click="logout">
+      {{ $t("app.logout.name") }}
+    </v-btn-->
+    <v-btn v-show="$route.name === 'home'" flat icon @click="togglePanels">
+      <v-icon>{{ toggleIcon }}</v-icon>
+    </v-btn>
     <v-btn v-show="$route.name === 'more'" flat icon @click="home">
       <v-icon>chevron_left</v-icon>
     </v-btn>
@@ -27,12 +28,26 @@ import { MODULES } from "@/store";
 
 export default {
   name: "TheNavigation",
-  computed: {
-    isAuthenticated() {
-      return this.$store.getters[MODULES.User.getters.isAuthenticated];
-    }
+  data: () => ({
+    toggleIcon: "expand_more"
+  }),
+  created() {
+    this.$eventHub.$on("update:toggleIcon", e => {
+      this.toggleIcon = e;
+    });
+  },
+  beforeDestroy() {
+    this.$eventHub.$off("update:toggleIcon");
   },
   methods: {
+    togglePanels() {
+      if (this.toggleIcon === "expand_more") {
+        this.toggleIcon = "expand_less";
+      } else {
+        this.toggleIcon = "expand_more";
+      }
+      this.$eventHub.$emit("update:togglePanels");
+    },
     home() {
       router.push({ name: "home" });
     },
