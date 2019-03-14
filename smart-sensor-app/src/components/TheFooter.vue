33579ff34d72556b0dcb706ae98c696d696cd29c
hunsalz <info@hunsalz.de>
2019-03-14 23:15:11 +0100

    removed copy & paste srcipt instructions

--- smart-sensor-app/src/components/TheFooter.vue
+++ smart-sensor-app/src/components/TheFooter.vue
@@ -15,29 +15,6 @@
   </v-footer>
 </template>
 
-<script>
-import router from "@/router";
-import { MODULES } from "@/store";
-
-export default {
-  name: "TheFooter",
-  computed: {
-    isAuthenticated() {
-      return this.$store.getters[MODULES.User.getters.isAuthenticated];
-    }
-  },
-  methods: {
-    home() {
-      router.push({ name: "home" });
-    },
-    logout() {
-      this.$store.dispatch(MODULES.User.actions.logout);
-    },
-    more() {
-      router.push({ name: "more" });
-    }
-  }
-};
-</script>
+<script></script>
 
 <style scoped></style>
