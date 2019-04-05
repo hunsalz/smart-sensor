4349ac74da5f0f1c7b5f5e127162e9ed0dd24429
hunsalz <info@hunsalz.de>
2019-04-05 14:13:54 +0200

    removed

--- smart-sensor-app/tests/unit/Login.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from "@vue/test-utils";
-import Login from "@/components/Login.vue";
-
-describe("Login.vue", () => {
-  it("renders data.email when passed", () => {
-    const wrapper = mount(Login);
-    expect(wrapper.vm.email).toBe("");
-  });
-});
